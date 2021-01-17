08a5f99f0f4c78a8b68ade6cbac71f13f3bfa692
Maciej Blok <mblok@student.agh.edu.pl>
2021-01-17 11:21:58 +0100

    ProductTest is working

--- src/main/java/pl/edu/agh/mwo/invoice/product/Product.java
+++ src/main/java/pl/edu/agh/mwo/invoice/product/Product.java
@@ -10,24 +10,32 @@ public abstract class Product {
     private final BigDecimal taxPercent;
 
     protected Product(String name, BigDecimal price, BigDecimal tax) {
+    	if (name == null || name.equals("") || price == null || price.signum() == -1) {	//obiekty porownujemy przez .equals, bo inaczej może posypać sie kiedys program (patrz internowanie stringów), referencje do null porownujemy zawsze "=="
+    		throw new IllegalArgumentException("You cannot create product with null or empty name or null or negate price.");
+    	}
+    	
+    	if (price == null || price.signum() == -1) {	//obiekty porownujemy przez .equals, bo inaczej może posypać sie kiedys program (patrz internowanie stringów), referencje do null porownujemy zawsze "=="
+    		throw new IllegalArgumentException("You cannot create product with null or empty name or null or negate price.");
+    	}
+    	
         this.name = name;
         this.price = price;
         this.taxPercent = tax;
     }
 
     public String getName() {
-        return null;
+        return this.name;
     }
 
     public BigDecimal getPrice() {
-        return null;
+        return this.price;
     }
 
     public BigDecimal getTaxPercent() {
-        return null;
+        return this.taxPercent;
     }
 
     public BigDecimal getPriceWithTax() {
-        return null;
+        return this.price.multiply(this.taxPercent).add(this.price );
     }
 }
